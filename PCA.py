f83001ba381a559f5f6c153d70fa0a223c5dba80
LejlaMesic <lejla.mesic@stud.uni-heidelberg.de>
2023-06-28 00:00:53 +0200

    created new file "test_knn_2" where knn is performed. current accuracy = 64% (k=5)

--- PCA.py
+++ PCA.py
@@ -1,14 +1,12 @@
 from sklearn.decomposition import PCA
-from Versuch_z_transformation import transformierte_werte
 from Versuch_z_transformation import transformierte_Matrix
 import numpy as np
-import matplotlib.pyplot as plt
 
-n_components = 20 #checked with scree plot (elobow method)
+n_components = 20 #checked with scree plot (elbow method)
 pca = PCA(n_components=n_components)
 
 #Fit the model to the transformed data
-pca.fit(transformierte_Matrix)
+pca.fit(transformierte_Matrix) 
 
 #Acess the principal components and the variance ratio
 principal_components = pca.components_  
@@ -17,7 +15,7 @@ explained_variance_ratio = pca.explained_variance_ratio_
 #Transform data to lower-dimensional space
 transformed_data = pca.transform(transformierte_Matrix)
 
-#Check if it worked
+#Test: Check if it worked
 # 1. Explained Variance Ratio
 print("Explained variance ratio for each component:")
 print(explained_variance_ratio)
@@ -25,3 +23,5 @@ print(explained_variance_ratio)
 # 2. Total Variance Explained
 total_variance_explained = np.sum(explained_variance_ratio) #85%
 print("Total variance explained:", total_variance_explained)
+#correlation matrix machen
+#eigenfaces dazufügen
