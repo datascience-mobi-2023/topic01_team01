588bc2c2722f043a12506b83ce6b289d60796de4
LejlaMesic <lejla.mesic@stud.uni-heidelberg.de>
2023-07-09 19:32:30 +0200

    all variables in _z_transf. changed to english

--- Versuch_z_transformation.py
+++ Versuch_z_transformation.py
@@ -1,25 +1,24 @@
-from matplotlib import transforms
 import numpy as np
 import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 import os
-import pandas as pd
 
 
-Ordner = "Bilder\Training"
-Pixelwerte = []
-for dateiname in os.listdir(Ordner): 
-    if dateiname.endswith("gif"): 
-        bildpfad = os.path.join(Ordner, dateiname)
-        bild = imageio.imread(bildpfad) 
-        bild_pixelwerte = bild.flatten() 
-        Pixelwerte.append(bild_pixelwerte)
-Matrix = np.column_stack(Pixelwerte) 
-print(Matrix) 
-print(Matrix.shape) 
+
+folder = "Bilder\Training"
+pixelValues = []
+for filename in os.listdir(folder): 
+    if filename.endswith("gif"): 
+        imagePath = os.path.join(folder, filename)
+        image = imageio.imread(imagePath) 
+        image_pixelValues = image.flatten() 
+        pixelValues.append(image_pixelValues)
+matrix = np.column_stack(pixelValues) 
+print(matrix) 
+print(matrix.shape) 
         
 #Plot 
-hist, edges = np.histogram(Matrix)
+hist, edges = np.histogram(matrix)
 plt.bar(edges[:-1], hist, width=0.9)
 plt.xlabel("Pixel values")
 plt.ylabel("Frequency")
@@ -38,21 +37,20 @@ def z_transformation(Matrix):
             transformed_row = (row - mean) / sd 
         transformed_matrix.append(transformed_row) 
     return transformed_matrix
-neue_matrix = Matrix
-transformierte_Matrix = z_transformation(neue_matrix) 
+new_matrix = matrix
+transformierte_Matrix = z_transformation(new_matrix) 
 
 # Convert all transformed values ​​to a flat array so I don't get 120 histograms output:
-transformierte_werte = np.concatenate(transformierte_Matrix)
-#transformierte_werte = transformierte_werte[~np.isnan(transformierte_werte)] #NaN-Werte (not a number) können auftreten, wenn sd=0, versucher ich noch zu beheben. Aber was genau sollen wir dann tun?
+transformed_values = np.concatenate(transformierte_Matrix)
 
 # Plot histogram for the transformed values
-hist, edges = np.histogram(transformierte_werte)
+hist, edges = np.histogram(transformed_values)
 plt.bar(edges[:-1], hist, width=0.9)
 plt.xlabel("Pixel values")
 plt.ylabel("Frequency")
 plt.title("Frequency of pixel values after transformation")
 plt.show()
-Mittelwert = np.mean(transformierte_werte)
-Standardabweichung = np.std(transformierte_werte)
-print("Mean of tansformed data : ", Mittelwert)
-print("Standard deviation of performed data", Standardabweichung)
+average = np.mean(transformed_values)
+standardDeviation = np.std(transformed_values)
+print("Mean of tansformed data : ", average)
+print("Standard deviation of performed data", standardDeviation)
